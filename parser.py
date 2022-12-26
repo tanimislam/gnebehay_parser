cd3c787319309ab6365811d1a8d378d2e60fbc8b
Tanim Islam <islam5@llnl.gov>
2022-12-25 17:31:42 -0800

    finally gotten the parser working with variables, with arbitrary length strings.

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
-import enum
-import re
+import enum, re, logging
+from itertools import chain
 
 
 class TokenType(enum.Enum):
@@ -10,35 +10,77 @@ class TokenType(enum.Enum):
     T_DIV = 4
     T_LPAR = 5
     T_RPAR = 6
-    T_END = 7
+    T_VAR  = 7
+    T_EXP  = 8
+    T_END = 9
 
 
-class Node:
+_mappings = {
+    '+': TokenType.T_PLUS,
+    '-': TokenType.T_MINUS,
+    '*': TokenType.T_MULT,
+    '/': TokenType.T_DIV,
+    '^': TokenType.T_EXP,
+    '(': TokenType.T_LPAR,
+    ')': TokenType.T_RPAR}
+
+def isint( tok ):
+    try:
+        val = int( tok )
+        return True
+    except:
+        return False
+
+    
+class Node( object ):
     def __init__(self, token_type, value=None):
         self.token_type = token_type
         self.value = value
         self.children = []
+    
+    def __str__( self ):
+        return '%s, %s, num_child = %d' % (
+            self.token_type, self.value, len( self.children ) )
 
+def get_token_mapping( tok ):
+    if tok in _mappings:
+        token_type = _mappings[ tok ]
+        return Node(token_type, value = tok )
+    #
+    if isint( tok ):
+        return Node(TokenType.T_NUM, value=int(tok))
+    #
+    if isinstance( tok, str ):
+        return Node(TokenType.T_VAR, tok )
+    raise Exception('Invalid token: {}'.format( tok ) )
+
+def get_left_string( tok_sub ):
+    def _get_left_paren( tok ):
+        if len( tok ) == 0: return '('
+        return tok.strip( )
+    return list(map(_get_left_paren, tok_sub.strip().split('(') ) )
 
 def lexical_analysis(s):
-    mappings = {
-        '+': TokenType.T_PLUS,
-        '-': TokenType.T_MINUS,
-        '*': TokenType.T_MULT,
-        '/': TokenType.T_DIV,
-        '(': TokenType.T_LPAR,
-        ')': TokenType.T_RPAR}
-
-    tokens = []
-    for c in s:
-        if c in mappings:
-            token_type = mappings[c]
-            token = Node(token_type, value=c)
-        elif re.match(r'\d', c):
-            token = Node(TokenType.T_NUM, value=int(c))
-        else:
-            raise Exception('Invalid token: {}'.format(c))
-        tokens.append(token)
+    splitstring = list(
+        filter(lambda elem: len(elem) > 0,
+               re.split(r'(\d+|\W+)', s.strip())))
+    splitstring = list( chain.from_iterable(
+        map(get_left_string, splitstring)))
+    #
+    ## because dumbass, I am ALSO doing a 
+    logging.debug( splitstring )
+    tokens = list(map(
+        get_token_mapping, splitstring ) )
+    # tokens = [ ]
+    # for c in s:
+    #     if c in mappings:
+    #         token_type = mappings[c]
+    #         token = Node(token_type, value=c)
+    #     elif re.match(r'\d', c):
+    #         token = Node(TokenType.T_NUM, value=int(c))
+    #     else:
+    #         raise Exception('Invalid token: {}'.format(c))
+    #    tokens.append(token)
     tokens.append(Node(TokenType.T_END))
     return tokens
 
@@ -74,9 +116,22 @@ def parse_e2(tokens):
     return left_node
 
 
-def parse_e3(tokens):
+def parse_e3( tokens ):
+    left_node = parse_e4(tokens)
+
+    while tokens[0].token_type in [TokenType.T_EXP, ]:
+        node = tokens.pop(0)
+        node.children.append(left_node)
+        node.children.append(parse_e4(tokens))
+        left_node = node
+
+    return left_node
+
+def parse_e4(tokens):
     if tokens[0].token_type == TokenType.T_NUM:
         return tokens.pop(0)
+    if tokens[0].token_type == TokenType.T_VAR:
+        return tokens.pop( 0 )
 
     match(tokens, TokenType.T_LPAR)
     expression = parse_e(tokens)
